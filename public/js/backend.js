6648ebb612e5db768f6c596889dc374f298e890d
Alain Eustaquio Rivera Rios <aln.r.r.91@outlook.com>
2022-11-06 00:32:49 -0600

    Update11062022

--- public/js/backend.js
+++ public/js/backend.js
@@ -1,8 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-analytics.js";
-import { getStorage, ref, uploadBytes } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-storage.js"
-import { getFirestore, collection, addDoc, getDocs, onSnapshot, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
+import { getStorage, ref, listAll, uploadBytes, uploadBytesResumable, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-storage.js"
+import { getFirestore, query, where, collection, addDoc, getDocs, onSnapshot, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -27,81 +27,118 @@ const storage = getStorage(app);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-// Agrega un nuevo objecto a la base de datos
-export async function addNewAmimal() {
-  let object = {
-    Nombre: document.querySelector("#nombre").value,
-    Talla: document.querySelector("#talla").value,
-    Edad: document.querySelector("#edad").value,
-    Color: document.querySelector("#color").value,
-    Sexo: document.querySelector("#sexo").value,
-    Imagen: "imagen",
-    Descripcion: document.querySelector("#descripcion").value,
-    Razon: document.querySelector("#razon").value
-  };
+var file, isUploading;
 
-  try {
-    const docRef = await addDoc(collection(db, "mascotas"), object);
-    console.log("Document written with ID: ", docRef.id);
-    alert("Mascota agregada correctamente");
-  } catch (e) {
-    console.error("Error adding document: ", e);
-    alert("Ocurrio un error.");
+// Agrega un nuevo objecto a la base de datos
+async function addNewAmimal() {
+  if (isUploading) {
+    alert("Please wait, thumbnail is uploading...");
+    return;
+  }
+  if (file != undefined) {
+    uploadFile(file);
+  }
+  else {
+    alert("Please select an image first!");
   }
 }
 
 window.addnew = addNewAmimal;
 
+// listen to the results of a query. This creates a query snapshot.
+// For example, to listen to the documents with Nombre Firulays:
+/*
+const q = query(collection(db, "mascotas"), where("Nombre", "==", "Firulays"));
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  querySnapshot.forEach((doc) => {
+    console.log(`Nombre: ${doc.data().Nombre}`);
+  });
+});
+*/
+
+const unsub = onSnapshot(collection(db, "mascotas"), (querySnapshot) => {
+  let tab;
+  tab = document.getElementById('tab-body');
+  if (tab == undefined) { unsub(); return;}
+
+  tab.innerHTML = ``; // clears current data
+  
+  querySnapshot.forEach((doc) => {
+    // adds each row filled with new data
+    tab.innerHTML += `
+    <tr>
+    <td>${doc.id}</td>
+    <td>${doc.data().Nombre}</td>
+    <td>${doc.data().Talla}</td>
+    <td>${doc.data().Edad}</td>
+    <td>${doc.data().Color}</td>
+    <td>${doc.data().Sexo}</td>
+    <td><img alt="image" class="thumbnail" src="${doc.data().Thumbnail}"></td>
+    <td>${doc.data().Descripcion}</td>
+    <td>${doc.data().Razon}</td>
+    <td>
+    <button data-id="${doc.id}" class="action-edit"><span class="icon-edit"></span>Editar</button>
+    <button data-id="${doc.id}" class="action-delete"><span class="icon-delete"></span>Eliminar</button>
+    </td>
+    </tr>
+    `;
+});
+
+const btn_delete = document.querySelectorAll('.action-delete');
+    const btn_update = document.querySelectorAll('.action-edit');
+    btn_delete.forEach(btn =>
+    {
+    btn.addEventListener('click', (e) => {
+      const docRef = doc(db, "mascotas", e.target.getAttribute('data-id'));
+      deleteDoc(docRef).then(() => {
+        alert("Elemento borrado!");
+        console.log("Entire Document has been deleted successfully.")
+    }).catch(error => {
+        console.log(error);
+    })
+    });
+    });
+
+});
+
+// reads data once
+/*
 const querySnapshot = await getDocs(collection(db, "mascotas"));
 querySnapshot.forEach((d) => {
   // doc.data() is never undefined for query doc snapshots
   // console.log(doc.id, " => ", doc.data());
-  let tab;
-tab = document.getElementById('tabs');
-tab.innerHTML += `
-    <tr>
-    <td>${d.id}</td>
-    <td>${d.data().Nombre}</td>
-    <td>${d.data().Talla}</td>
-    <td>${d.data().Edad}</td>
-    <td>${d.data().Color}</td>
-    <td>${d.data().Sexo}</td>
-    <td>${d.data().Imagen}</td>
-    <td>${d.data().Descripcion}</td>
-    <td>${d.data().Razon}</td>
-    <td>
-    <button data-id="${d.id}" class="action-edit"><span class="icon-edit"></span>Editar</button>
-    <button data-id="${d.id}" class="action-delete"><span class="icon-delete"></span>Eliminar</button>
-    </td>
-    </tr>
-    `;
-
-    const btns = document.querySelectorAll('.action-delete');
-const btn_update = document.querySelectorAll('.action-edit');
-btns.forEach(btn =>
-{
-btn.addEventListener('click', (e) => {
-  const docRef = doc(db, "mascotas", e.target.getAttribute('data-id'));
-
-deleteDoc(docRef)
-.then(() => {
-  alert("Elemento borrado!");
-    console.log("Entire Document has been deleted successfully.")
-})
-.catch(error => {
-    console.log(error);
-})
 });
+*/
+
+// retrieve multiple documents with one request by querying documents in a collection
+// and query for all of the documents that meet a certain condition
+/*
+const q = query(collection(db, "mascotas"), where("Nombre", "==", "Firulays"));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
 });
+*/
 
+const filepicker = document.getElementById('upload-photo');
+if (filepicker != undefined) {
+  filepicker.addEventListener('change', (event) => {
+    const files = event.target.files;
+    for (const f of files) {
+      file = f;
+    }
+  });
+}
 
-});
-
-export function uploadFile(file) {
+function uploadFile(file) {
   // Create a reference to 'images/mountains.jpg'
-  const storageRef = ref(storage, "images/mountains.jpg");
-  // Upload the file and metadata
-  const uploadTask = uploadBytesResumable(storageRef, file);
+  const storageRef = ref(storage, "mascotas/" + file.name);
+  const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        // Upload the file and metadata
+        isUploading = true;
+  const uploadTask = uploadBytesResumable(storageRef, reader.result);
   // Register three observers:
   // 1. 'state_changed' observer, called any time the state changes
   // 2. Error observer, called on failure
@@ -123,6 +160,7 @@ export function uploadFile(file) {
       }
     },
     (error) => {
+     isUploading = false;
       // A full list of error codes is available at
       // https://firebase.google.com/docs/storage/web/handle-errors
       switch (error.code) {
@@ -139,8 +177,29 @@ export function uploadFile(file) {
     },
     () => {
       // Upload completed successfully, now we can get the download URL
-      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-        console.log("File available at", downloadURL);
+      getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+       isUploading = false;
+        //console.log("File available at", downloadURL);
+
+        let object = {
+          Nombre: document.querySelector("#nombre").value,
+          Talla: document.querySelector("#talla").value,
+          Edad: document.querySelector("#edad").value,
+          Color: document.querySelector("#color").value,
+          Sexo: document.querySelector("#sexo").value,
+          Thumbnail: downloadURL,
+          Descripcion: document.querySelector("#descripcion").value,
+          Razon: document.querySelector("#razon").value
+        };
+      
+        try {
+          const docRef = await addDoc(collection(db, "mascotas"), object);
+          console.log("Document written with ID: ", docRef.id);
+          alert("Mascota agregada correctamente");
+        } catch (e) {
+          console.error("Error adding document: ", e);
+          alert("Ocurrio un error.");
+        }
       });
     }
   );
@@ -153,5 +212,35 @@ export function uploadFile(file) {
 
   // Cancel the upload
   //uploadTask.cancel();
+      });
+      reader.readAsArrayBuffer(file);
 }
 
+// Create a reference under which you want to list
+const listRef = ref(storage, 'mascotas/perros');
+// Find all the prefixes and items.
+listAll(listRef)
+  .then((res) => {
+    res.prefixes.forEach((folderRef) => {
+      // All the prefixes under listRef.
+      // You may call listAll() recursively on them.
+    });
+    res.items.forEach((itemRef) => {
+      // All the items under listRef.
+
+      // Delete the file
+      /*
+      deleteObject(itemRef)
+        .then(() => {
+          // File deleted successfully
+          console.log("ok");
+        })
+        .catch((error) => {
+          // Uh-oh, an error occurred!
+          console.log("fail");
+        });
+        */
+    });
+  }).catch((error) => {
+    // Uh-oh, an error occurred!
+  });
--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,40 @@
-const toggleMenuOpen = () => document.body.classList.toggle("open");
 // ######## OAuth API
 hello.init({
     facebook: "605612360973350",
-    google: "679313092033-2j8qabl5e3dt2agp8c1aso4a736ghsos.apps.googleusercontent.com"
+    google: "679313092033-2j8qabl5e3dt2agp8c1aso4a736ghsos.apps.googleusercontent.com",
+    instagram: "",
+    github: "",
+    windows: "",
+    twitter: "",
+    linkedin: "",
+    dropbox: "",
+    yahoo: "",
+    soundcloud: "",
+    joinme: "",
+    foursquare: "",
+    flickr: "",
+    vk: "",
+    tumblr: "",
+    bikeindex: "",
+    box: ""
 },
 {
     redirect_uri: "https://dog-cat-7b3a6.web.app/"
 });
 
+// Triggered whenever session changes
+hello.on("auth", function(auth)
+{
+});
+
+// Triggered whenever a user logs in
 hello.on("auth.login", function(auth)
 {
     // Call user information, for the given network
     hello(auth.network).api("me").then(function(p)
     {
-        window.location.href = "options.html";
+        if (window.location.pathname != "/admin.html")
+        window.location.href = "admin.html";
         // p.first_name,
         // p.last_name,
         // p.email,
@@ -21,22 +42,39 @@ hello.on("auth.login", function(auth)
     });
 });
 
-// Logout button
-const logout = function()
+// Remove a callback. Both event name and function must exist.
+// hello.off('auth.login', sessionStart);
+
+// Triggered whenever a user logs out
+hello.on("auth.logout", function(auth)
 {
-    hello("google").logout().then(function()
+    // Call user information, for the given network
+    hello(auth.network).api("me").then(function(p)
     {
         window.location.href = "index.html";
-    }, function(e)
-    {
-        console.log("logout error: " + e.error.message);
     });
-}
-// sign in usin Google button
-//initOAuth("google");
-// sign in usin Facebook button
-//initOAuth("facebook");
+});
 
+// Triggered prior to requesting an authentication flow
+hello.on("auth.init", function(auth)
+{
+});
+
+// Triggered whenever a users credentials change
+hello.on("auth.update", function(auth)
+{
+});
+
+// Logout button
+const logout = (network) => hello(network).logout().then(function()
+{
+    window.location.href = "index.html";
+}, function(e)
+{
+    console.log("logout error: " + e.error.message);
+});
+
+// 
 function initOAuth(network, force = false) {
     // Make a login call and handle the response using promises
     var hi = hello(network);
@@ -70,18 +108,18 @@ function initOAuth(network, force = false) {
 let map, marker;
 async function initMap()
 {
-let data =
-{
-latitud: parseFloat("20.485193215567346"),
-longitud: parseFloat("-99.21841097091944"),
-nombre: "Ubicacion"
-}
-// Asigna las coordenadas: longitud, latitud a la constante coords
-// Mustra el mapa con las coordenadas asignadas
+    let data =
+    {
+        latitud: parseFloat("20.485193215567346"),
+        longitud: parseFloat("-99.21841097091944"),
+        nombre: "Ubicacion"
+    }
+    // Asigna las coordenadas: longitud, latitud a la constante coords
+    // Mustra el mapa con las coordenadas asignadas
 map = new google.maps.Map(document.getElementById("map"),
 {
-center: { lat: data.latitud, lng: data.longitud }, // centra el mapa en las coordenadas asignadas a la constante
-zoom: 17,
+    center: { lat: data.latitud, lng: data.longitud }, // centra el mapa en las coordenadas asignadas a la constante
+    zoom: 17,
 });
 
 // Especifica un marcador personalizado con el tamano determinado
@@ -94,25 +132,27 @@ const icon = {
 
 // Agrega un marcador al mapa
 marker = new google.maps.Marker({
-draggable: true,
-animation: google.maps.Animation.DROP,
-position: { lat: data.latitud, lng: data.longitud }, // centra el marcador en las coordenadas asignadas a la constante
-map,
-icon: icon,
-title: data.nombre, // Agrega un titulo la marcador
+    draggable: true,
+    animation: google.maps.Animation.DROP,
+    position: { lat: data.latitud, lng: data.longitud }, // centra el marcador en las coordenadas asignadas a la constante
+    map,
+    icon: icon,
+    title: data.nombre, // Agrega un titulo la marcador
 });
 }
+
+const toggleMenuOpen = () => document.body.classList.toggle("open");
 
 // ########## Email / Password validations
 function validateEmail(email)
 {
-  if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
-    alert("Please enter a valid email address")
-    return false;
-} else {
-    alert("login");
-    return true;
-}
+    if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
+        alert("Please enter a valid email address")
+        return false;
+    } else {
+        alert("login");
+        return true;
+    }
 }
 
 function select(kind) {
