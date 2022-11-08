7ef6c32cf14ab627823bbe0f9a8ce49068b50c79
Alain Eustaquio Rivera Rios <aln.r.r.91@outlook.com>
2022-11-08 17:31:20 -0600

    Update11082022

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,40 @@
-
-const toggleMenuOpen = () => document.body.classList.toggle("open");
 // ######## OAuth API
 hello.init({
     facebook: "605612360973350",
-    google: "679313092033-2j8qabl5e3dt2agp8c1aso4a736ghsos.apps.googleusercontent.com"
+    google: "679313092033-2j8qabl5e3dt2agp8c1aso4a736ghsos.apps.googleusercontent.com",
+    instagram: "",
+    github: "",
+    windows: "",
+    twitter: "",
+    linkedin: "",
+    dropbox: "",
+    yahoo: "",
+    soundcloud: "",
+    joinme: "",
+    foursquare: "",
+    flickr: "",
+    vk: "",
+    tumblr: "",
+    bikeindex: "",
+    box: ""
 },
 {
     redirect_uri: "https://dog-cat-7b3a6.web.app/"
 });
 
+// Triggered whenever session changes
+hello.on("auth", function(auth)
+{
+});
+
+// Triggered whenever a user logs in
 hello.on("auth.login", function(auth)
 {
     // Call user information, for the given network
     hello(auth.network).api("me").then(function(p)
     {
-        alert("Hola" + p.first_name);
+        if (window.location.pathname != "/admin.html")
+        window.location.href = "admin.html";
         // p.first_name,
         // p.last_name,
         // p.email,
@@ -22,22 +42,39 @@ hello.on("auth.login", function(auth)
     });
 });
 
-// Logout button
-const logout = function()
-{
-    hello("google").logout().then(function()
-    {
-        window.location.href = "/";
-    }, function(e)
-    {
-        console.log("logout error: " + e.error.message);
-    });
-}
-// sign in usin Google button
-//initOAuth("google");
-// sign in usin Facebook button
-//initOAuth("facebook");
+// Remove a callback. Both event name and function must exist.
+// hello.off('auth.login', sessionStart);
 
+// Triggered whenever a user logs out
+hello.on("auth.logout", function(auth)
+{
+    // Call user information, for the given network
+    hello(auth.network).api("me").then(function(p)
+    {
+        window.location.href = "index.html";
+    });
+});
+
+// Triggered prior to requesting an authentication flow
+hello.on("auth.init", function(auth)
+{
+});
+
+// Triggered whenever a users credentials change
+hello.on("auth.update", function(auth)
+{
+});
+
+// Logout button
+const logout = (network) => hello(network).logout().then(function()
+{
+    window.location.href = "index.html";
+}, function(e)
+{
+    console.log("logout error: " + e.error.message);
+});
+
+// 
 function initOAuth(network, force = false) {
     // Make a login call and handle the response using promises
     var hi = hello(network);
@@ -71,39 +108,6 @@ function initOAuth(network, force = false) {
 let map, marker;
 async function initMap()
 {
-<<<<<<< HEAD
-let data =
-{
-latitud: parseFloat("20.485193215567346"),
-longitud: parseFloat("-99.21841097091944"),
-nombre: "Ubicacion"
-}
-// Asigna las coordenadas: longitud, latitud a la constante coords
-// Mustra el mapa con las coordenadas asignadas
-map = new google.maps.Map(document.getElementById("map"),
-{
-center: { lat: data.latitud, lng: data.longitud }, // centra el mapa en las coordenadas asignadas a la constante
-zoom: 17,
-});
-
-// Especifica un marcador personalizado con el tamano determinado
-const icon = {
-    url: "../images/dog-cat.gif", // url
-    scaledSize: new google.maps.Size(60, 60), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor
-};
-
-// Agrega un marcador al mapa
-marker = new google.maps.Marker({
-draggable: true,
-animation: google.maps.Animation.DROP,
-position: { lat: data.latitud, lng: data.longitud }, // centra el marcador en las coordenadas asignadas a la constante
-map,
-icon: icon,
-title: data.nombre, // Agrega un titulo la marcador
-});
-=======
     let data = {
       latitud: parseFloat("20.485193215567346"),
       longitud: parseFloat("-99.21841097091944"),
@@ -133,19 +137,20 @@ title: data.nombre, // Agrega un titulo la marcador
       icon: icon,
       title: data.nombre, // Agrega un titulo la marcador
     });
->>>>>>> Update11082022
 }
+
+const toggleMenuOpen = () => document.body.classList.toggle("open");
 
 // ########## Email / Password validations
 function validateEmail(email)
 {
-  if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
-    alert("Please enter a valid email address")
-    return false;
-} else {
-    alert("login");
-    return true;
-}
+    if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(email))) {
+        alert("Please enter a valid email address")
+        return false;
+    } else {
+        alert("login");
+        return true;
+    }
 }
 
 function select(kind) {
@@ -157,8 +162,40 @@ function select(kind) {
     }
 }
 
+function closeDialog() {
+    document.querySelector(".modal").style.display = "none";
+}
 
-function newdoc(koud){
-    if(koud=="conoceme"){
-window.location.href="Description_cats.html"
+function showDialog() {
+    document.querySelector(".modal").style.display = "block";
+}
+
+function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      [...ev.dataTransfer.items].forEach((item, i) => {
+        // If dropped items aren't files, reject them
+        if (item.kind === 'file') {
+          const file = item.getAsFile();
+          console.log(`${file.name}`);
+        }
+      });
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      [...ev.dataTransfer.files].forEach((file, i) => {
+        console.log(`… file[${i}].name = ${file.name}`);
+      });
     }
+  }
+
+  function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+  
