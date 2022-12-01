35d1c640a62fd72510ee58f9b10784e51cff1438
Jonan Mendoza Chavarria <m.ch.jonan7@gmail.com>
2022-11-30 23:46:58 -0600

    donaciones

--- public/js/donaciones.js
+++ public/js/donaciones.js
@@ -1,84 +1,17 @@
 $(document).ready(function () {
-    mostrarMascotas();
+    mostrarDonacion();
 });
 
-function addMascota() {
+function addDonacion() {
     console.log("agregar mascotas");
-    let form = new FormData($("#formParte1")[0]);
-    console.log(form);
-    $.ajax({
-        data: form,
-        url:'php/addMascota.php',
-        type: "POST",
-        contentType: false,
-        processData: false,
-        beforesend: function () {},
-        success: function(response) {
-            var data2 = JSON.parse(response);
-            console.log(data2);
-            if (data2.response == "SUCCESS") {
-                console.log("SUCCESS");
-                alert(data2.detail);
-                window.location.reload();
-              } else {
-                alert('Ha ocurrido un error, intetelo mas tarde');
-              }
-        }
-    });
-}
-
-function mostrarMascotas() {
-    console.log("mostra mascota");
-    $.ajax({
-        data: {I:"D"},
-        url:'php/selectMascota.php',
-        type: "POST",
-        contentType: false,
-        processData: false,
-        beforesend: function () {},
-        success: function(response) {
-            var data2 = JSON.parse(response);
-            console.log(data2);
-            if (data2!=null) {
-                //Si se obtuvieron los datos correctos serán mostrados en pantalla
-                if (data2.response == "SUCCESS") {
-                  console.log("SUCCESS");
-                  let tab = document.getElementById('tab-body');
-                  let values = data2.detail;
-                  values.forEach(element => {
-                    tab.innerHTML += `
-                        <tr>
-                            <td>${element.nombre}</td>
-                            <td>${element.talla}</td>
-                            <td>${element.edad}</td>
-                            <td>${element.color}</td>
-                            <td>${element.sexo}</td>
-                            <td>${element.esterilizado}</td>
-                            <td><img alt="image" class="thumbnail" src="${element.foto}"></td>
-                            <td>${element.raza}</td>
-                            <td>
-                                <button onclick="deleteMascota(${element.id})" class="action-delete"><span class="icon-delete"></span>Eliminar</button>
-                            </td>
-                        </tr>
-                    `;
-                  });
-                } else {
-                  alert('Ha ocurrido un error, vuelva mas tarde');
-                  window.location= 'index.php';
-                }
-            }else{
-                alert('Ha ocurrido un error, intetelo mas tarde');
-                window.location= 'index.php';
-            }
-        }
-    });
-}
-
-function deleteMascota(idMascota) {
-    console.log("eliminar mascotas");
-    console.log(idMascota);
-
-    $.post('php/deleteMascota.php',{id:idMascota},function(data){
+    let values = {
+        valNom:document.querySelector('#nombre').value,
+        valTel:document.querySelector('#edad').value,
+        valDon:document.querySelector('#talla').value,
+        valDes:document.querySelector('#valDes').value
+    };
+    console.log(values);
+    $.post('php/addDonacion.php',values,function(data){
         //Aqui se obtiene la respuesta 
         var data2 = JSON.parse(data);
         console.log(data2);
@@ -99,3 +32,68 @@ function deleteMascota(idMascota) {
     });
 }
 
+function mostrarDonacion() {
+    console.log("mostra mascota");
+    $.ajax({
+        data: {I:"D"},
+        url:'php/selectDonacion.php',
+        type: "POST",
+        contentType: false,
+        processData: false,
+        beforesend: function () {},
+        success: function(response) {
+            var data2 = JSON.parse(response);
+            console.log(data2);
+            if (data2!=null) {
+                //Si se obtuvieron los datos correctos serán mostrados en pantalla
+                if (data2.response == "SUCCESS") {
+                  console.log("SUCCESS");
+                  let tab = document.getElementById('tab-body');
+                  let values = data2.detail;
+                  values.forEach(element => {
+                    tab.innerHTML += `
+                        <tr>
+                            <td>${element.nombre}</td>
+                            <td>${element.telefono}</td>
+                            <td>${element.tipo_donacion}</td>
+                            <td>${element.descripcion}</td>
+                            <td>${element.fechaF}</td>
+                            <td>
+                                <button onclick="deleteDonacion(${element.id})" class="action-delete"><span class="icon-delete"></span>Eliminar</button>
+                            </td>
+                        </tr>
+                    `;
+                  });
+                } else {
+                  alert('Ha ocurrido un error, vuelva mas tarde');
+                  window.location= 'index.php';
+                }
+            }else{
+                alert('Ha ocurrido un error, intetelo mas tarde');
+                window.location= 'index.php';
+            }
+        }
+    });
+}
+
+function deleteDonacion(idDonacion) {
+    $.post('php/deleteDonacion.php',{id:idDonacion},function(data){
+        //Aqui se obtiene la respuesta 
+        var data2 = JSON.parse(data);
+        console.log(data2);
+        if (data2!=null) {
+            //Si se obtuvieron los datos correctos serán mostrados en pantalla
+            if (data2.response == "SUCCESS") {
+              console.log("SUCCESS");
+              alert(data2.detail);
+              window.location.reload();
+            } else {
+              alert('Ha ocurrido un error, intetelo mas tarde');
+              window.location.reload();
+            }
+        }else{
+            alert('Ha ocurrido un error, intetelo mas tarde');
+            window.location= 'index.php';
+        }
+    });
+}
